6c78448568148a767c340dcd9abf38881b639fb6
Martin Vilcans <martin@librador.com>
2016-04-16 14:19:20 +0200

    Floor that you can jump on

---
+++ Assets/GameMain.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameMain : MonoBehaviour {
+
+    public GameObject[] prefabList;
+
+    public Vector2 startPosition;
+
+    private Dictionary<string, GameObject> prefabs;
+
+    public void Start() {
+
+        prefabs = new Dictionary<string, GameObject>();
+        for(int i = 0; i < prefabList.Length; ++i) {
+            GameObject obj = prefabList[i];
+            prefabs[obj.name] = obj;
+        }
+
+        CreateLevel();
+        CreatePlayer();
+    }
+
+    private void CreateLevel() {
+        GameObject floorPrefab = prefabs["Square"];
+        for(int col = 0; col < 4; ++col) {
+            GameObject newObj = (GameObject)Instantiate(
+                floorPrefab,
+                new Vector2(col, 0),
+                Quaternion.identity
+            );
+        }
+    }
+
+    private void CreatePlayer() {
+        GameObject player = (GameObject)Instantiate(prefabs["Circle"], startPosition, Quaternion.identity);
+        player.name = "Player";
+        player.AddComponent<Player>();
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Player : MonoBehaviour {
+
+    private float jumpVelocity = 3;
+    private float horizontalVelocity = .25f;
+    private float verticalVelocity;
+    private bool onGround;
+
+    private const float distanceFromGround = .5f;
+
+    private int groundCollisions = 0;
+
+    void FixedUpdate() {
+        if(!onGround) {
+            transform.position += Vector3.up * verticalVelocity * Time.deltaTime;
+            verticalVelocity += Physics2D.gravity.y * Time.deltaTime;
+        }
+        transform.position += Vector3.right * horizontalVelocity * Time.deltaTime;
+
+        if(onGround && Input.GetButton("Jump")) {
+            verticalVelocity = jumpVelocity;
+            onGround = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        ++groundCollisions;
+        Debug.LogFormat("Collided with {0}, now {1}", other, groundCollisions);
+        onGround = true;
+        verticalVelocity = 0;
+        transform.position = new Vector2(transform.position.x, other.bounds.max.y + distanceFromGround);
+
+        //transform.position = 
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        --groundCollisions;
+        Assert.IsTrue(groundCollisions >= 0);
+        Debug.LogFormat("Exited {0}, now {1}", other, groundCollisions);
+        if(groundCollisions == 0) {
+            onGround = false;
+            Debug.Log("No longer on ground");
+        }
+    }
+}
