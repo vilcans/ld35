17edf98b1ffcf3a4ec5ab25eef88dfe02cbad4c2
Martin Vilcans <martin@librador.com>
2016-04-17 20:00:21 +0200

    Start vertices for square shape at 0 degrees to be consistent with stars

--- Assets/Shapes/Shapes.cs
+++ Assets/Shapes/Shapes.cs
@@ -3,10 +3,14 @@
 public static class Shapes {
 
     public static Vector3[] squareVertices = ShapeGenerator.CreateMeshPoints(new Vector3[] {
-        new Vector3(-.5f, -.5f, 0),
-        new Vector3(.5f, -.5f, 0),
-        new Vector3(.5f, .5f, 0),
-        new Vector3(-.5f, .5f, 0),
+        new Vector3(.5f, 0, 0),  // right
+        new Vector3(.5f, .5f, 0),  // top right
+        new Vector3(.0f, .5f, 0),  // top
+        new Vector3(-.5f, .5f, 0),  // top left
+        new Vector3(-.5f, .0f, 0),  // left
+        new Vector3(-.5f, -.5f, 0),  // bottom left
+        new Vector3(-.0f, -.5f, 0),  // bottom
+        new Vector3(.5f, -.5f, 0),  // bottom right
     });
 
     public static Vector3[] circleVertices = ShapeGenerator.CreateStarPoints(.5f, .5f);
